4138b02febfbf78e09d7b023bbbad658fe5b115d
Stephanie Kim <47402894+steph-kimm@users.noreply.github.com>
2025-11-19 01:09:19 -0500

    Add professional settings overview hub

--- web/haastia/src/routes/routeConfig.js
+++ web/haastia/src/routes/routeConfig.js
@@ -20,7 +20,7 @@ import ProfessionalCustomers from "../views/professionalView/ProfessionalCustome
 import StripeConnectPage from "../views/professionalView/StripeConnectPage";
 import ProfessionalOnboarding from "../views/professionalView/ProfessionalOnboarding";
 import ProfessionalSettings from "../views/professionalView/ProfessionalSettings";
-import ProfessionalSettingsLanding from "../views/professionalView/ProfessionalSettingsLanding";
+import ProfessionalSettingsOverview from "../views/professionalView/ProfessionalSettingsOverview";
 
 export const customerRouteConfig = [
   { path: "/", component: Home },
@@ -48,5 +48,5 @@ export const professionalRouteConfig = [
   { path: "/bookings", component: ProfessionalRequests },
   { path: "/bookings/:bookingId", component: AppointmentDetails },
   { path: "/payments/connect", component: StripeConnectPage },
-  { path: "/pro/settings", component: ProfessionalSettingsLanding },
+  { path: "/pro/settings", component: ProfessionalSettingsOverview },
 ];
